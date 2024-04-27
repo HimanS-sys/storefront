433a8bf2d63685cf21b86e684b02da798f857161
Himanshu <kandpallucky@gmail.com>
2024-04-27 23:44:23 +0530

    setup mysql db

--- storefront/settings.py
+++ storefront/settings.py
@@ -86,12 +86,15 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "storefront",
+        "HOST" : "localhost",
+        "USER" : "root",
+        "PASSWORD" : "root",
     }
 }
 
-
+ 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
